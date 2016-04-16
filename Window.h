a54cc899329c6baabda5ff2afccadc5182f976ab
Aschalin <lukasz.bryniarski@gmail.com>
2016-04-16 19:29:32 +0200

    change of includes

--- Window.h
+++ Window.h
@@ -1,9 +1,9 @@
 #ifndef WINDOW_H_
 #define WINDOW_H_
-#include "SDL.h"
-#include "SDL_opengl.h"
-#include "SDL_image.h"
-#include "SDL_ttf.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_opengl.h"
+#include "SDL2/SDL_image.h"
+#include "SDL2/SDL_ttf.h"
 
 class Window {
 protected:
