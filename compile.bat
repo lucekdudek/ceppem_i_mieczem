e29d3d6d5e57a9879284f4f3f59e50328f25ac16
michall12 <michall1100@gmail.com>
2016-04-16 13:20:43 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
 set PATH=%PATH%;c:\MinGW\bin
-mkdir bin
 g++ *.h *.cpp -o bin\ceppem.exe -IC:\MinGW\include\SDL2 -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ 
