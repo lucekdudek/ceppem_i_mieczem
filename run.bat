9093b268e2c2b418f7e6e52f2d21cc28acf088d2
Michal <michall1100@gmail.com>
2016-04-16 13:24:24 +0200

    Added run.bat.

--- run.bat
+++ run.bat
@@ -1,2 +1,7 @@
+@echo off
+echo Kompilowanie
 set PATH=%PATH%;c:\MinGW\bin
 g++ *.h *.cpp -o bin\ceppem.exe -IC:\MinGW\include\SDL2 -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ 
+echo Uruchamianie
+cd bin
+ceppem.exe
