96a5324ef0770fbbaeea3e9ae1f1473a4c91e0ba
Aschalin <lukasz.bryniarski@gmail.com>
2016-04-16 20:48:54 +0200

    change of directories structure

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Kompilowanie
 set PATH=%PATH%;c:\MinGW\bin
-g++ *.h *.cpp -o bin\ceppem.exe -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ 
+g++ src\*\*.h src\*\*.cpp src\main.cpp -o bin\ceppem.exe -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include "View/Window.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    Window w;
+    cout << "OK" << endl;
+    //system("pause");
+
+    return 0;
+}
