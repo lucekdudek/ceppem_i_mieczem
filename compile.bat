23810186b88d98f3f1b778a4fa93a9064bbc8f65
michall12 <michall1100@gmail.com>
2016-04-16 20:27:36 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -2,3 +2,4 @@
 echo Kompilowanie
 set PATH=%PATH%;c:\MinGW\bin
 g++ *.h *.cpp -o bin\ceppem.exe -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ 
+cd ..
