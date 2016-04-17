efcb348b051c52b9ff03919a22c5f1d22c4f5e3f
Michal <michall1100@gmail.com>
2016-04-17 14:53:41 +0200

    Updated Window class.

--- src/view/window.h
+++ src/view/window.h
@@ -12,39 +12,28 @@ protected:
     SDL_GLContext mainGLContext;
 public:
     Window();
-
-    virtual ~Window();
-
-    void eventLoop();
-
-    void drawImage(int x, int y, int textureId, int width = 0, int height = 0);
+    ~Window();
 
     int done;
 
+    void eventLoop();
+    void drawImage(int x, int y, int textureId, int width = 0, int height = 0);
     int loadGLTexture(char *fileName);
-
     TTF_Font *loadFont(char *fileName, int fontSize);
-
     int renderText(char *text, TTF_Font *text_font);
-
+    void renderFrame();
 private:
     SDL_Cursor *cursor1;
     SDL_Cursor *cursor2;
     int previousTime;
     int currentTime;
     unsigned int *textures;
+    int w, h;
 
     void init(int width, int height, char *title);
-
     void initSDL(Uint32 width, Uint32 height, char *title, Uint32 flags);
-
     static Uint32 gameLoopTimer(Uint32 interval, void *param);
-
     void createOrthoProj(int width, int height);
-
-    void renderFrame();
-
-    int w, h;
 };
 
 const int MAX_FPS = 60;
