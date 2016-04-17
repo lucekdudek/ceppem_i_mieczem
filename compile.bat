10b824a5db34defdad1620143cb66241e25d4dce
Michal <michall1100@gmail.com>
2016-04-17 23:27:11 +0200

    Fixed compile.bat.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Kompilowanie
 set PATH=%PATH%;c:\MinGW\bin
-g++ src\*\*.h src\*\*.cpp src\main.cpp -o bin\ceppem.exe -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ > compilation.log 2>&1
+g++ src\main.cpp src\controller\controller.cpp src\controller\fpshandler.cpp src\controller\timer.cpp src\controller\controller.h src\controller\fpshandler.h src\controller\timer.h src\model\element.cpp src\model\model.cpp src\model\element.h src\model\model.h src\rapidxml-1.13\rapidxml.hpp src\rapidxml-1.13\rapidxml_print.hpp src\rapidxml-1.13\rapidxml_utils.hpp src\view\view.cpp src\view\window.cpp src\view\view.h src\view\window.h -o bin\ceppem.exe -lmingw32 -lopengl32 -lSDL2_ttf -lSDL2_image -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ -std=c++11 > compilation.log 2>&1
 pause
