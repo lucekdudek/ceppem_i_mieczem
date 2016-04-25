6696cdadde13b2cf5f3f44882a29cf9b59448fae
Aschalin <lukasz.bryniarski@gmail.com>
2016-04-25 22:40:43 +0200

    Added management of button raised events

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include "controller.h"
 #include "fpshandler.h"
 
 bool Controller::running = true;
 Window Controller::window;
 Model Controller::model;
+std::string Controller::current_view;
 
 Controller::Controller()
 {
@@ -16,14 +18,9 @@ Controller::~Controller()
 
 }
 
-void Controller::event(std::string event_name)
-{
-	std::cout<<event_name<<std::endl;
-}
-
 void Controller::run()
 {
-	window.setView(model.getXml());
+    changeView("mainmenu");
     FpsHandler clock;
     while (running)
     {
@@ -34,7 +31,64 @@ void Controller::run()
     }
 }
 
-void Controller::setDone()
+void Controller::event(std::string event_name)
+{
+    if(current_view == "mainmenu")
+    {
+        mainMenuEvent(event_name);
+    }
+    else if(current_view =="new_game")
+    {
+        newGameEvent(event_name);
+    }
+    else
+    {
+        std::cout<<"error, view \""<<current_view<<"\" is invalid\n";
+    }
+}
+
+void Controller::mainMenuEvent(std::string event_name)
+{
+    if(event_name == "NEW_GAME")
+    {
+        startNewGame();
+    }
+    else if(event_name == "EXIT_GAME")
+    {
+        setDone();
+    }
+    else
+    {
+        std::cout<<"error, unexpected command:"<<std::setw(10)<<event_name<<std::endl;
+    }
+}
+
+void Controller::newGameEvent(std::string event_name)
+{
+    if(event_name == "NEW_GAME")
+    {
+        changeView("mainmenu");
+    }
+    else
+    {
+        std::cout<<"error, unexpected command:"<<std::setw(10)<<event_name<<std::endl;
+    }
+}
+
+void Controller::startNewGame()
+{
+    std::cout<<"new game starting\n";
+    changeView("new_game");
+}
+
+void Controller::changeView(std::string view)
+{
+    std::cout<<"changed view to: "<<std::setw(10)<<view<<std::endl;
+    current_view = view;
+    window.setView(model.getXml());
+}
+
+inline void Controller::setDone()
 {
     running = false;
 }
--- src/controller/controller.h
+++ src/controller/controller.h
@@ -12,11 +12,17 @@ public:
     ~Controller();
     static void event(std::string event_name);
     static void run();
-    static void setDone();
+    inline static void setDone();
 private:
     static bool running;
     static Window window;
     static Model model;
+    static std::string current_view;                 //zawiera informację o aktualnie wyświetlanym widoku.
+    static void startNewGame();
+    static void mainMenuEvent(std::string event_name);
+    static void newGameEvent(std::string event_name);
+    inline static void changeView(std::string view);
+
 };
 
 #endif //CEPEM_I_MIECZEM_CONTROLLER_H
