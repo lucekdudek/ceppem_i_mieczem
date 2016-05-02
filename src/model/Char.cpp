aa37a76593d32b625eac0b015e138a32a35b26c6
Aschalin <lukasz.bryniarski@gmail.com>
2016-05-02 22:47:21 +0200

    Added initial model of character.

---
+++ src/model/Char.cpp
@@ -0,0 +1,89 @@
+//
+// Created by Aschali on 02.05.2016.
+//
+
+#include "Char.h"
+
+
+int Char::getStrenght()
+{
+    return strenght;
+}
+
+int Char::getDexterity()
+{
+    return dexterity;
+}
+
+int Char::getAgility()
+{
+    return agility;
+}
+
+int Char::getWisdom()
+{
+    return wisdom;
+}
+
+int Char::getInteligence()
+{
+    return inteligence;
+}
+
+int Char::getCharisma()
+{
+    return charisma;
+}
+
+void Char::setStrenght(int value)
+{
+    strenght = value;
+}
+
+void Char::setDexterity(int value)
+{
+    dexterity = value;
+}
+
+void Char::setAgility(int value)
+{
+    agility = value;
+}
+
+void Char::setWisdom(int value)
+{
+    wisdom = value;
+}
+
+void Char::setInteligence(int value)
+{
+    inteligence = value;
+}
+
+void Char::setCharisma(int value)
+{
+    charisma = value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
